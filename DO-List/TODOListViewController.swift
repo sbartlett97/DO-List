9893d4dc49b48167259984e6e6b82ca119a171fb
NomcidalManiac1 <sam.bartlett858@gmail.com>
2018-11-28 11:07:22 +0000

    Base table view created with default values, and ability to check/uncheck items on the list

---
+++ DO-List/TODOListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DO-List
+//
+//  Created by Samuel Bartlett on 28/11/2018.
+//  Copyright © 2018 Samuel Bartlett. All rights reserved.
+//
+
+import UIKit
+
+class TODOListViewController: UITableViewController {
+    
+    let itemArray: [String] = ["Christmas Shopping","Food Shopping","Coursework","Job Applications"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    //MARK - TableView Data Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
