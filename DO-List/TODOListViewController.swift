20a2deaf995af4ec2e05c1966905744f2c1799c8
NomcidalManiac1 <sam.bartlett858@gmail.com>
2018-11-28 12:06:28 +0000

    Added comments and improved readability

--- DO-List/TODOListViewController.swift
+++ DO-List/TODOListViewController.swift
@@ -13,43 +13,73 @@ class TODOListViewController: UITableViewController {
     var itemArray: [String] = ["Christmas Shopping","Food Shopping","Coursework","Job Applications"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
     //MARK - TableView Data Methods
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //pull the default cell out of the storyboard
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        //set text value of the cell to required item (currently from array)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //allow a dynamic table view so items can be added and displayed
         return itemArray.count
     }
     
     //MARK - TableView Delegate Methods
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //debug current cell clicked
         print(itemArray[indexPath.row])
+        
+        //add or remove checkmarks
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
+        
+        //unhighlight the cell after press
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     //MARK - Add new Items to list
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        var textField: UITextField = UITextField()
+        
+        //Create a textField that is globally accessible by the method
+        var textField = UITextField()
+        
+        //set up the alert
         let alert = UIAlertController(title: "Add new TODO Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //
+            
+            //check if the text field is blank
             if textField.text != nil{
+                
+                //if not add item to the array and update the tableView
                 self.itemArray.append(textField.text!)
                 self.tableView.reloadData()
             }
         }
+        
+        //Add a text field to the alert
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
+            
+            //make the text field globaly accessible to the method
             textField = alertTextField
         }
+        
+        //add the action to the alert and present the alert
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
